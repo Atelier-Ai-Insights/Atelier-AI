087f8e4825f1977577f43227ce48f6a48fe83a9d
Atelier-Ai-Insights <insights@atelier-consulting.com>
2026-01-14 13:11:36 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -8,15 +8,15 @@ MODE_ETNOCHAT = "Análisis Multimodal"
 MODE_TREND_ANALYSIS = "Análisis de Tendencias" 
 
 # --- Modos de Evaluación ---
-MODE_IDEA_EVAL = "Evaluar una idea"
+MODE_IDEA_EVAL = "Evaluación de ideas"
 MODE_IMAGE_EVAL = "Evaluación Visual"
 MODE_VIDEO_EVAL = "Evaluación de Video"
 
 # --- Modos de Reportes ---
 MODE_REPORT = "Generar un reporte de reportes"
-MODE_ONEPAGER = "Generador de One-Pager PPT"
+MODE_ONEPAGER = "Generador de One-Pager"
 
 # --- Modos de Creatividad ---
-MODE_IDEATION = "Conversaciones creativas"
-MODE_CONCEPT = "Generación de conceptos"
+MODE_IDEATION = "Conversaciones Creativas"
+MODE_CONCEPT = "Generación de Conceptos"
 MODE_SYNTHETIC = "Perfiles Sintéticos"
